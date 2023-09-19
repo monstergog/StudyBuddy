fe6afdcd3b3d84c1036d19b58d08f6a00e116256
Alex N <59707701+monstergog@users.noreply.github.com>
2023-09-18 19:27:47 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ const startApolloServer = async () => {
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+      console.log(`Use GraphQL at https://study-buddy-p3-0d14fa2a37d6.herokuapp.com${server.graphqlPath}`);
     })
   })
   };
